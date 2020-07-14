5ba7e4964b4d5dffcbea0c0f0f8372a558fe6fbf
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-07-14 20:02:40 +0100

    fix: CosmosDB ouputs

--- azurerm/modules/azurerm-cosmosdb/outputs.tf
+++ azurerm/modules/azurerm-cosmosdb/outputs.tf
@@ -1,11 +1,6 @@
-output "cosmosdb_database_name" {
-  description = "CosmosDB Database name"
-  value       = var.create_cosmosdb ? azurerm_cosmosdb_sql_database.default.0.name : ""
-}
-
 output "cosmosdb_account_name" {
   description = "CosmosDB account name"
-  value       = var.create_cosmosdb ? azurerm_cosmosdb_sql_database.default.0.account_name : ""
+  value       = var.create_cosmosdb ? azurerm_cosmosdb_account.default.0.account_name : ""
 }
 
 output "cosmosdb_endpoint" {
@@ -17,4 +12,9 @@ output "cosmosdb_primary_master_key" {
   description = "Primary Key for accessing the DB CRUD, should only be used in applications running outside of AzureCloud"
   sensitive   = true
   value       = var.create_cosmosdb ? azurerm_cosmosdb_account.default.0.primary_master_key : ""
+}
+
+output "cosmosdb_database_name" {
+  description = "CosmosDB database name"
+  value       = var.create_cosmosdb ? azurerm_cosmosdb_sql_database.default.name : ""
 }
