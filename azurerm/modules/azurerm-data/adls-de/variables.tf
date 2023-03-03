cfde8b07028913f3d92ad8bf603c31a98c4d8ba1
jakub.slon <jakubslon@gmail.com>
2023-03-03 14:18:52 +0000

    feat/ refactor-module

--- azurerm/modules/azurerm-data/adls-de/variables.tf
+++ azurerm/modules/azurerm-data/adls-de/variables.tf
@@ -4,6 +4,24 @@ variable "region" {
   default     = "northeurope"
 }
 
+variable "account_kind" {
+  type        = string
+  description = "(OPTIONAL) Defines the Kind of account - available options are: BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2. Changing the account_kind value from Storage to StorageV2 will not trigger a force new on the storage account, it will only upgrade the existing storage account from Storage to StorageV2 keeping the existing storage account in place."
+  default     = "StorageV2"
+}
+
+variable "account_tier" {
+  type        = string
+  description = "value"
+  default     = "Defines the Tier to use for this storage account. Valid options are Standard and Premium. For BlockBlobStorage and FileStorage accounts only Premium is valid. Changing this forces a new resource to be created."
+}
+
+variable "hns_enabled" {
+  type        = bool
+  description = "Is Hierarchical Namespace enabled? This can be used with Azure Data Lake Storage Gen 2 (see here for more information). Changing this forces a new resource to be created."
+  default     = true
+}
+
 variable "resource_group_name" {
   type        = string
   description = "Resource Group Name"
@@ -17,7 +35,7 @@ variable "adls_account_replication_type" {
 
 variable "adls_containers" {
   type        = set(string)
-  description = "ADLS containers to create"
+  description = "ADLS containers to create, for example: curated, staging, raw"
   default     = ["curated", "staging", "raw"]
 }
 
@@ -25,4 +43,67 @@ variable "resource_namer" {
   description = "This should be a uniformly created string - ideally using something like cloudposse label module to ensure conventions on naming are followed throughout organization. this value is used in all the places within the module to name resources - additionally it changes the string to ensure it conforms to Azure standards where appropriate - i.e. blob/KV/ACR names are stripped of non alphanumeric characters and in some cases strings are sliced to conform to max char length"
   type        = string
   default     = "genericname"
+}
+
+############################################
+# NAMING
+############################################
+
+variable "name_company" {
+  description = "Company Name - should/will be used in conventional resource naming"
+  type        = string
+}
+
+variable "name_project" {
+  description = "Project Name - should/will be used in conventional resource naming"
+  type        = string
+}
+
+variable "name_component" {
+  description = "Component Name - should/will be used in conventional resource naming. Typically this will be a logical name for this part of the system i.e. `API` || `middleware` or more generic like `Billing`"
+  type        = string
+}
+
+variable "name_environment" {
+  type = string
+}
+
+variable "stage" {
+  type    = string
+  default = "dev"
+}
+
+variable "attributes" {
+  description = "Additional attributes for tagging"
+  default     = []
+}
+
+variable "tags" {
+  description = "Tags to be assigned to all resources, NB if global tagging is enabled these will get overwritten periodically"
+  type        = map(string)
+  default     = {}
+}
+
+
+variable "resource_group_location" {
+  type    = string
+  default = "uksouth"
+}
+
+
+# Each region must have corresponding a shortend name for resource naming purposes 
+variable "location_name_map" {
+  type = map(string)
+
+  default = {
+    northeurope   = "eun"
+    westeurope    = "euw"
+    uksouth       = "uks"
+    ukwest        = "ukw"
+    eastus        = "use"
+    eastus2       = "use2"
+    westus        = "usw"
+    eastasia      = "ase"
+    southeastasia = "asse"
+  }
 }
